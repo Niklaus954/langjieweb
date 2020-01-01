79b49a5c0b8a6bc9be4567edaa12d2b5752ea918
Hanan18313 <631179992@qq.com>
2020-01-01 21:51:45 +0800

    200101

--- src/components/Home/Activity.jsx
+++ src/components/Home/Activity.jsx
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-import React, { Component }  from 'react';
-import apiAboutLangjie from'../../api/apiAboutLangjie'
-import { withRouter } from 'react-router-dom'
 
-const Activity = ({parentLocation}) => {
-    console.log(parentLocation);
-
-    return <div>
-        123
-    </div>
-=======
 import React, { useEffect, useState }  from 'react';
 import apiAboutLangjie from'../../api/apiAboutLangjie';
 import FadeTransitions from '../Common/FadeTransitions';
@@ -115,31 +104,6 @@ const Activity = ({history}) => {
             </div>
         </FadeTransitions>
     )
->>>>>>> suggestReading
 }
 
 export default withRouter(Activity);
-
-// class Activity extends Component {
-
-//     async componentWillMount() {
-//         const result = await apiAboutLangjie.fetchRecentActivity({
-//             page: 1,
-//             pageSize: 20,
-//         });
-//         console.log(result);
-//     }
-
-//     render() {
-//         return (
-//             <div>活动</div>
-//         )
-//     }
-// }
-
-
-<<<<<<< HEAD
-// export default Activity;
-=======
-export default withRouter(Activity);
->>>>>>> suggestReading
