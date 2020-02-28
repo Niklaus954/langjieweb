07f23148653edb5ee85f526bd713cf5e77a1228d
Niklaus954 <hz_zhangligang@163.com>
2020-02-28 17:00:21 +0800

    合同，卡服务更新

--- src/components/Service/Contract.jsx
+++ src/components/Service/Contract.jsx
@@ -17,32 +17,109 @@ const Contract = props => {
     const [album, setAlbum] = useState([]);
     const isPc = useMediaQuery(CONFIG.minDeviceWidth);
 
-    let delCol = [ 'id', 'snLackNum', 'isdel', 'album', 'madeInApp', 'update_time', 'update_person', 'insert_time', 'insert_person', 'complete'];
-
     const backSelectedItem = async item => {
         if (isPc) {
             const result = await apiService.getContractInfo(item);
             const resAlbum = result.data.comment.filter(item => item.column_name === 'album')
             setAlbum(resAlbum)
-            result.data.comment.forEach((items, index) => {
-                if (items.column_name === 'install') {
-                    items.val = items.val == 0 ? '否' : '是';
-                } else if (items.column_name === 'isFreeze') {
-                    if (items.val == 0) {
-                        delCol.push('freeze_reason');
-                        delCol.push('freeze_start_time');
-                        delCol.push('freeze_time');
-                    }
-                    items.val = items.val == 0 ? '否' : '是';
-                } else if (items.column_name === 'contract_state') {
-                    if (items.val != '关闭') {
-                        delCol.push('close_reason');
-                        delCol.push('close_time');
-                    }
+            const labelObj = {
+                contract_no: {
+                    name: '合同编号',
+                },
+                cus_abb: {
+                    name: '购方',
+                },
+                purchase: {
+                    name: '购方采购',
+                },
+                contract_state: {
+                    name: '合同状态',
+                },
+                sale_person: {
+                    name: '销售员',
+                },
+                sign_time: {
+                    name: '签订日期',
+                },
+                total_amount: {
+                    name: '总金额',
+                },
+                payable: {
+                    name: '应付金额',
+                },
+                paid: {
+                    name: '已付金额',
+                },
+                install: {
+                    name: '需要安装',
+                },
+                isDirectSale: {
+                    name: '是否直销',
+                },
+                delivery_state: {
+                    name: '发货状态',
+                },
+                delivery_time: {
+                    name: '发货时间',
+                },
+                take_person: {
+                    name: '收货确认人',
+                },
+                take_time: {
+                    name: '收货确认时间',
+                },
+                isFreeze: {
+                    name: '是否冻结',
+                },
+                freeze_reason: {
+                    name: '冻结原因',
+                },
+                freeze_start_time: {
+                    name: '冻结开始时间',
+                },
+                freeze_time: {
+                    name: '冻结截止日期',
+                },
+                close_reason: {
+                    name: '关闭原因',
+                },
+                close_time: {
+                    name: '关闭日期',
+                },
+                other: {
+                    name: '其他约定',
+                },
+            };
+            for (let i = 0; i < result.data.comment.length; i++) {
+                const key = result.data.comment[i].column_name;
+                if (labelObj[key]) labelObj[key].val = result.data.comment[i].val;
+                if(key === 'isFreeze' && result.data.comment[i].val == 0) {
+                    delete labelObj.freeze_reason;
+                    delete labelObj.freeze_start_time;
+                    delete labelObj.freeze_time;
                 }
-            });
-            delCol = [ ...new Set(delCol) ];
-            const renderList = result.data.comment.filter(items => delCol.indexOf(items.column_name) === -1);
+                if(key === 'contract_state' && result.data.comment[i].val !== '关闭') {
+                    delete labelObj.close_reason;
+                    delete labelObj.close_time;
+                }
+                if (key === 'install' || key === 'isFreeze' || key === 'isDirectSale') {
+                    if(labelObj[key].val==0){
+                        labelObj[key].val = '否';
+                    }else{
+                        labelObj[key].val = '是';
+                    }
+                }else if(key==='take_time'){
+                    if(labelObj[key].val==='0000-00-00'||labelObj[key].val==null) labelObj[key].val = '';
+                }
+            };
+            const renderList = [];
+            for (const key in labelObj) {
+                renderList.push({
+                    column_name: key,
+                    column_comment: labelObj[key].name,
+                    val: labelObj[key].val,
+                });
+            }
             setInfoList(renderList);
         } else {
             props.history.push({
--- src/components/Service/VirInfo.jsx
+++ src/components/Service/VirInfo.jsx
@@ -17,39 +17,7 @@ const VirInfo = props => {
 
     const fetch = async serialNo => {
         const result = await apiService.fetchVirCardInfo({serialNo});
-        const source = [
-            { column_name: 'serialNo', column_comment: '序列号', val: '' },
-            { column_name: 'model', column_comment: '型号', val: '' },
-            { column_name: 'validTime', column_comment: '有效期', val: '' },
-            { column_name: 'latestRegNo', column_comment: '注册码', val: '' },
-            { column_name: 'regAuth', column_comment: '授权码', val: '' },
-            { column_name: 'regAppName0', column_comment: '自费app', val: '' },
-            { column_name: 'appValidTime0', column_comment: '有效期', val: '' },
-            { column_name: 'appRegCode0', column_comment: '注册码', val: '' },
-            { column_name: 'appRegAuth0', column_comment: '授权码', val: '' },
-            { column_name: 'fwVer', column_comment: '固件版本', val: '' },
-            { column_name: 'regVer', column_comment: '注册版本', val: '' },
-            // { column_name: 'dealer', column_comment: '中间商', val: '' },
-            { column_name: 'oemUser', column_comment: '指定配套用户', val: '' },
-            { column_name: 'authType', column_comment: '授权类型', val: '' },
-            { column_name: 'VBGN', column_comment: '名义起始期', val: '' },
-            { column_name: 'VEND', column_comment: '名义有效期', val: '' },
-            { column_name: 'machineNo', column_comment: '机器码', val: '' },
-            { column_name: 'chnlNum', column_comment: '通道数', val: '' },
-            { column_name: 'caliCoeff', column_comment: '标定系数', val: '' },
-            { column_name: 'ad2Mode', column_comment: 'AD采集模式', val: '' },
-            { column_name: 'pulseMode', column_comment: 'PM脉冲模式', val: '' },
-            { column_name: 'vibFreq', column_comment: 'DA伺服颤振频率', val: '' },
-            { column_name: 'vibAmp', column_comment: 'DA伺服颤振幅值', val: '' },
-            { column_name: 'SPWM_AC_AMP', column_comment: 'SPWM交流幅值', val: '' },
-            { column_name: 'SSI_MODE', column_comment: 'SSI模式', val: '' },
-            { column_name: 'HOURS', column_comment: '已用小时数', val: '' },
-            { column_name: 'remark', column_comment: '附注', val: '' },
-        ];
-        source.forEach((items, index) => {
-            items.val = result.data[items.column_name];
-        });
-        setInfoList(source);
+        setInfoList(result.data);
     }
 
     return (
