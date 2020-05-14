119b62547c1ef9d6d35ae718c90d05a8a6f8905a
Hanan18313 <631179992@qq.com>
2020-05-14 13:59:18 +0800

    pc 产品布局

--- src/components/Service/VirCard.jsx
+++ src/components/Service/VirCard.jsx
@@ -8,150 +8,139 @@ import {
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import FadeTransitions from '../Common/FadeTransitions'
 import { List, ListView } from 'antd-mobile';
-import { Paper, InputBase, Divider, makeStyles, TableBody, Table, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import DirectionsIcon from '@material-ui/icons/Directions';
-
-const columns = [
-    {id: 'album', label: '', align: 'left', minWidth: 100},
-    {id: 'serial_number', label: '序列号', align: 'left', minWidth: 100},
-    {id: 'serial_type', label: '型号', align: 'left', minWidth: 100},
-    {id: 'validity', label: '有效期', align: 'left', minWidth: 100}
-]
-
-const toggleOptions = []
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        padding: '2px 4px',
-        display: 'flex',
-        alignItems: 'center',
-    },
-    container: {
-        maxHeight: 540,
-    },
-    input: {
-        marginLeft: theme.spacing(1),
-        flex: 1,
-    },
-    iconButton: {
-        padding: 10,
-    },
-    divider: {
-        height: 28,
-        margin: 4,
-    },
-}))
+import ParagraphStyles from "../Common/ParagraphStyles";
+import AppBar from '@material-ui/core/AppBar';
+import {Tabs, Tab, Paper } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import SwipeableViews from 'react-swipeable-views';
+import PropTypes from 'prop-types'
+import { useTheme, makeStyles } from '@material-ui/core/styles';
 
 const Item = List.Item;
+const tabArr = ['生产信息', '销售信息', '注册历史', '保修单']
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+    return(
+        <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`full-width-tabpanel-${index}`}
+        aria-labelledby={`full-width-tab-${index}`}
+        {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    )
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+  
+function a11yProps(index) {
+    return {
+        id: `full-width-tab-${index}`,
+        'aria-controls': `full-width-tabpanel-${index}`,
+    };
+}
+  
+const useStyles = makeStyles((theme) => ({
+    root: {
+        backgroundColor: theme.palette.background.paper,
+        width: '100%',
+    },
+}));
 
 const VirCard = props => {
     const [infoList, setInfoList] = useState([]);
-    const [page, setPage] = useState(0)
-    const [pageSize, setPageSize] = useState(10)
-    const [data, setData] = useState([])
+    const [album, setAlbum] = useState([])
     const isPc = useMediaQuery(CONFIG.minDeviceWidth);
     const classes = useStyles();
-    useEffect(() => {
-        const fetch = async() => {
-            const result = await apiService.fetchVirCard({
-                page: page,
-                pageSize: pageSize,
-                keywords: ''
-            })
-            if(result.code === 200) setData(result.data)
+    const theme = useTheme();
+    const [value ,setValue] = useState(0)
+
+    const backSelectedItem = async item => {
+        if (isPc) {
+            const result = await apiService.fetchVirCardInfo(item)
+            console.log(result)
+            setInfoList(result.data)
+        } else {
+            props.history.push({
+                pathname: '/virInfo/' + item.serialNo, 
+            });
         }
-        fetch()
-    },[page, pageSize])
-    
-    const SearchBar = () => {
-        return(
-            <div>
-                <Paper component="form" className={classes.root} style={{width: isPc ? 500 : 300}}>
-                    <IconButton className={classes.iconButton} aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <InputBase
-                        className={classes.input}
-                        placeholder="请输入产品序列号、型号"
-                        inputProps={{ 'aria-label': '请输入产品序列号、型号' }}
-                    />
-                    <IconButton type="submit" className={classes.iconButton} aria-label="search">
-                        <SearchIcon />
-                    </IconButton>
-                    {/* <Divider className={classes.divider} orientation="vertical" />
-                    <IconButton color="primary" className={classes.iconButton} aria-label="directions">
-                        <DirectionsIcon />
-                    </IconButton> */}
-                </Paper>
+    }
+
+    const renderList = items => {
+        const validTime = items.validTime === 0 ? '永久注册' : items.validTime;
+        return (
+            <div style={{ flex: 1, padding: 4, marginLeft: 4 }}>
+                <p>序列号: {items.serialNo}</p>
+                <p>型号: {items.model}</p>
+                <p>有效日期: {validTime}</p>
             </div>
         )
     }
 
-    //mobile
-    const renderMobileList = () => {
-        if(data.length === 0) return
-        const resArr = []
-        data.map((item, index) => {
-            const validTime = item.validTime === 0 ? '永久注册' : item.validTime;
-            resArr.push(<div key={index} onClick={() => {props.history.push({pathname: '/virInfo/' + item.serialNo })}}>
-                <div style={{display: 'flex', flexDirection: 'row'}}>
-                    <div><img src={item.album} alt=""/></div>
-                    <div>
-                    <p>序列号: {item.serialNo}</p>
-                    <p>型号: {item.model}</p>
-                    <p>有效日期: {validTime}</p>
-                    </div>
-                </div>
-                <Divider variant='middle'/>
-            </div>)
-        })
-        return resArr
-    }
-
-    const renderPcList = () => {
-        const handleOnMouseEnter = (index) => {
-            window.document.getElementById(index).style.background = '#eee'
-        }
-        const handleOnMouseLeave = (index) => {
-            window.document.getElementById(index).style.background = 'none'
-        }
-        if(data.length === 0) return
-        const resArr = []
-        data.forEach((item, index) => {
-            const validTime = item.validTime === 0 ? '永久注册' : item.validTime;
-            resArr.push(<div key={index} id={index} style={{display: 'flex', flexDirection: 'row'}} onMouseEnter={() => {handleOnMouseEnter(index)}} onMouseLeave={() => {handleOnMouseLeave(index)}} onClick={() => {props.history.push({pathname: '/virInfo/' + item.serialNo })}}>
-                <div><img src={item.album} alt=""/></div>
-                <div>
-                    <p>序列号：{item.serialNo}</p>
-                    <p>型号：{item.model}</p>
-                    <p>有效日期：{validTime}</p>
-                </div>
-            </div>)
-        })
-        return resArr
-    }
-
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+      };
+    
+      const handleChangeIndex = (index) => {
+        setValue(index);
+      };
 
     return (
         <FadeTransitions>
-            <div>{isPc ? 
-                <div>
-                    <div>
-                        <div className="top_img" style={{display:'flex', justifyContent: 'center'}}>{isPc ? <img src="http://iph.href.lu/800x150" alt=""/> : <img src="http://iph.href.lu/300x80" alt=""/>}</div>
-                        <div className="search" style={{display:'flex', justifyContent: 'center', margin: '20px 0'}}><SearchBar/></div>
+            <div style={{ width: '100%', height: '100%', display: 'flex', borderRight: '1px solid #eee' }}>
+                <div style={{ width: isPc ? 400 : '100%', overflow: 'auto' }}>
+                    <ItemList
+                        isPc={isPc}
+                        fetchList={apiService.fetchVirCard}
+                        renderAlbum={true}
+                        renderList={renderList}
+                        backSelectedItem={backSelectedItem}
+                    ></ItemList>
+                </div>
+                {isPc ? <div style={{margin: '0 auto'}}>
+                    <div className="img" style={{display: 'flex', justifyContent: 'center', margin: '20px 0'}}>
+                        <img src="http://iph.href.lu/240x160?text=产品图片" alt=""/>
                     </div>
-                    <div style={{height: 500, overflow: 'auto', width: '70%', margin: '10px auto 10px auto'}}>{renderPcList()}</div>
-                </div> : 
-                <div style={{ display: 'flex', justifyContent: 'center',flexDirection: 'column' }}>
-                    <div style={{background: '#fff', position: 'fixed', top: 44, width: '100%', paddingTop: '10px'}}>
-                        <div className="top_img" style={{display:'flex', justifyContent: 'center'}}>{isPc ? <img src="http://iph.href.lu/800x150" alt=""/> : <img src="http://iph.href.lu/300x80" alt=""/>}</div>
-                        <div className="search" style={{display:'flex', justifyContent: 'center', margin: '20px 0'}}><SearchBar/></div>
+                    <div className={classes.root}>
+                        <AppBar position="static" color="default">
+                            <Tabs
+                            value={value}
+                            onChange={handleChange}
+                            indicatorColor="primary"
+                            textColor="primary"
+                            variant="fullWidth"
+                            aria-label="full width tabs example"
+                            >
+                            {tabArr.map((tab, index) => (
+                                <Tab key={index} label={tab} {...a11yProps(index)} />
+                            ))}
+                            </Tabs>
+                        </AppBar>
+                        <SwipeableViews
+                            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                            index={value}
+                            onChangeIndex={handleChangeIndex}
+                        >
+                            {tabArr.map((tab, index) => (
+                                <TabPanel key={index} value={value} index={index} dir={theme.direction}>{tab}</TabPanel>
+                            ))}
+                        </SwipeableViews>
                     </div>
-                <div style={{paddingTop: '160px'}}>{renderMobileList()}</div>
-            </div>}</div>
+                </div> : <div></div>}
+            </div>
         </FadeTransitions>
     )
 }
