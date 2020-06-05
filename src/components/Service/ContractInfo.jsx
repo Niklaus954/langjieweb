e498cd30c41077c75f306c34fb05d2751ee82a4b
Niklaus954 <hz_zhangligang@163.com>
2020-06-05 15:24:57 +0800

    合同数据结构变动

--- src/components/Service/ContractInfo.jsx
+++ src/components/Service/ContractInfo.jsx
@@ -19,109 +19,20 @@ const ContractInfo = props => {
 
     const fetch = async contract_no => {
         const result = await apiService.getContractInfo({contract_no});
-        const resAlbum = result.data.comment.filter(item => item.column_name === "album");
-        if(!resAlbum[0].val){
-            resAlbum[0].val = '/controller_system.png'
+        const albumData = [{ column_name: 'album', column_comment: '图片', val: '/controller_system.png' }];
+        const album = result.data.data.album;
+        if (album) {
+            albumData[0].val = album;
         }
-        setAlbum(resAlbum)
-        const labelObj = {
-            contract_no: {
-                name: '合同编号',
-            },
-            cus_abb: {
-                name: '购方',
-            },
-            purchase: {
-                name: '购方采购',
-            },
-            contract_state: {
-                name: '合同状态',
-            },
-            sale_person: {
-                name: '销售员',
-            },
-            sign_time: {
-                name: '签订日期',
-            },
-            total_amount: {
-                name: '总金额',
-            },
-            payable: {
-                name: '应付金额',
-            },
-            paid: {
-                name: '已付金额',
-            },
-            install: {
-                name: '需要安装',
-            },
-            isDirectSale: {
-                name: '是否直销',
-            },
-            delivery_state: {
-                name: '发货状态',
-            },
-            delivery_time: {
-                name: '发货时间',
-            },
-            take_person: {
-                name: '收货确认人',
-            },
-            take_time: {
-                name: '收货确认时间',
-            },
-            isFreeze: {
-                name: '是否冻结',
-            },
-            freeze_reason: {
-                name: '冻结原因',
-            },
-            freeze_start_time: {
-                name: '冻结开始时间',
-            },
-            freeze_time: {
-                name: '冻结截止日期',
-            },
-            close_reason: {
-                name: '关闭原因',
-            },
-            close_time: {
-                name: '关闭日期',
-            },
-            other: {
-                name: '其他约定',
-            },
-        };
-        for (let i = 0; i < result.data.comment.length; i++) {
-            const key = result.data.comment[i].column_name;
-            if (labelObj[key]) labelObj[key].val = result.data.comment[i].val;
-            if(key === 'isFreeze' && result.data.comment[i].val == 0) {
-                delete labelObj.freeze_reason;
-                delete labelObj.freeze_start_time;
-                delete labelObj.freeze_time;
-            }
-            if(key === 'contract_state' && result.data.comment[i].val !== '关闭') {
-                delete labelObj.close_reason;
-                delete labelObj.close_time;
-            }
-            if (key === 'install' || key === 'isFreeze' || key === 'isDirectSale') {
-                if(labelObj[key].val==0){
-                    labelObj[key].val = '否';
-                }else{
-                    labelObj[key].val = '是';
-                }
-            }else if(key==='take_time'){
-                if(labelObj[key].val==='0000-00-00'||labelObj[key].val==null) labelObj[key].val = '';
-            }
-        };
+        setAlbum(albumData)
         const renderList = [];
-        for (const key in labelObj) {
+        result.data.label.forEach(items => {
             renderList.push({
-                column_name: key,
-                column_comment: labelObj[key].name,
-                val: labelObj[key].val,
+                column_name: items.key,
+                column_comment: items.name,
+                val: items.val,
             });
-        }
+        });
         setInfoList(renderList);
     }
 
